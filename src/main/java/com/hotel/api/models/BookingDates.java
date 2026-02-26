abae6ce27e431d46a13fe506a78082fff0576af3
regfaradpro <regis.faradji@gmail.com>
2026-02-26 10:38:25 +0100

    Initial commit: Directory structure

---
+++ src/main/java/com/hotel/api/models/BookingDates.java
@@ -0,0 +1,17 @@
+package com.hotel.api.models;
+
+public class BookingDates {
+    private String checkin;
+    private String checkout;
+
+    public BookingDates() {}
+    public BookingDates(String checkin, String checkout) {
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+    // Getters et Setters
+    public String getCheckin() { return checkin; }
+    public void setCheckin(String checkin) { this.checkin = checkin; }
+    public String getCheckout() { return checkout; }
+    public void setCheckout(String checkout) { this.checkout = checkout; }
+}
